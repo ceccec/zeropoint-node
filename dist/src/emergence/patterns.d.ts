c8f5a224d6a8cd62ad2849a5b4d969a1be6b20ff
Tsvetan Rouschev <675674+ceccec@users.noreply.github.com>
2025-07-08 12:22:27 +0300

    Remove dist folder from git tracking - build artifacts should be ignored

--- dist/src/emergence/patterns.d.ts
+++
@@ -1,7 +0,0 @@
-export interface MetaphysicalPattern {
-    name: string;
-    signature: RegExp;
-    context: string;
-}
-export declare const metaphysicalPatterns: MetaphysicalPattern[];
-//# sourceMappingURL=patterns.d.ts.map
